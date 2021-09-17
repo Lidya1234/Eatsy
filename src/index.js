d8ca49bf738b54cc2d3c46c639f4f31d99e57dc1
Lidya1234 <lidya94gher@gmail.com>
2021-09-17 14:04:04 +0300

    Make import to store as default

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import { store } from './reducers/store'
-import { Provider } from 'react-redux'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import App from './App';
+import store from './reducers/store';
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
-)
-
+  document.getElementById('root'),
+);
