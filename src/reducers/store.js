88c28327628a62311e341660213b1e59bfeb8aaf
Lidya1234 <lidya94gher@gmail.com>
2021-10-05 14:03:15 +0300

    Add dispatch to fetchOrderList and addOrderList

--- src/reducers/store.js
+++ src/reducers/store.js
@@ -1,15 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
 import foodReducer, { fetchFoodList } from './foodSlice';
 import userReducer, { logUserIn } from './userSlice';
+import orderReducer, { fetchOrderList, addOrderList } from './cartSlice';
 
 const store = configureStore({
   reducer: {
     food: foodReducer,
     user: userReducer,
+    order: orderReducer,
   },
 });
-
+const order = {
+  customer_id: 1,
+  food_id: 1,
+};
 store.dispatch(fetchFoodList());
 store.dispatch(logUserIn({ email: 'admin@email.com', password: 1234 }));
-
+store.dispatch(fetchOrderList());
+store.dispatch(addOrderList(order));
 export default store;
