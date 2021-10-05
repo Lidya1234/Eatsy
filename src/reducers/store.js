d9c82d3796f78c2051ab29a4c8aa90db6aa60e53
helman101 <helmanandresortegon@gmail.com>
2021-10-05 12:16:09 +0200

    add cosole logs to test API calls

--- src/reducers/store.js
+++ src/reducers/store.js
@@ -1,12 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import foodReducer, { fetchFoodList } from './foodSlice';
+import userReducer, { logUserIn } from './userSlice';
 
 const store = configureStore({
   reducer: {
     food: foodReducer,
+    user: userReducer,
   },
 });
 
 store.dispatch(fetchFoodList());
+store.dispatch(logUserIn({ email: 'admin@email.com', password: 1234 }));
 
 export default store;
