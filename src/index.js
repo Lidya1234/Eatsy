2d78068ad3dffe3a614e6ad766f5a86f92b4b0b6
helman101 <helmanandresortegon@gmail.com>
2021-10-06 11:51:43 +0200

    add Routes component and set up toastify

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { Provider } from 'react-redux';
+import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import App from './App';
+import './index.css';
+import Routes from './routes/Routes';
 import store from './reducers/store';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Routes />
   </Provider>,
   document.getElementById('root'),
 );
