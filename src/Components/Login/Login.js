eb2577a0e7b7d712157697ce9203bbb1339706d3
helman101 <helmanandresortegon@gmail.com>
2021-10-06 11:51:58 +0200

    create log in component

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logUserIn } from '../../reducers/userSlice';
 import './login.scss';
 
-const Login = () => (
-  <div className="login">
-    <h1>Login</h1>
-  </div>
-);
+const Login = ({ dispatch }) => {
+  const [userEmail, setUserEmail] = useState('');
+  const [userPassword, setUserPassword] = useState('');
 
-export default Login;
+  const handleOnChange = (e) => {
+    if (e.target.tagName === 'INPUT') {
+      if (e.target.type === 'email') {
+        setUserEmail(e.target.value);
+      } else if (e.target.type === 'password') {
+        setUserPassword(e.target.value);
+      }
+    }
+  };
+
+  const handleSubmit = () => {
+    const params = { email: userEmail, password: userPassword };
+    dispatch(logUserIn(params));
+  };
+
+  return (
+    <div className="login">
+      <h1>Login</h1>
+      <div onChange={handleOnChange}>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button type="button" onClick={handleSubmit}>Log in</button>
+      </div>
+    </div>
+  );
+};
+
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+export default connect()(Login);
