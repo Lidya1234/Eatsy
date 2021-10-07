80d9a2e22379eeaec045e7c7e0599365bc51e589
helman101 <helmanandresortegon@gmail.com>
2021-10-07 12:22:55 +0200

    add validations and toast

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { toast } from 'react-toastify';
+import { toastDark } from '../../toast/toastSetup';
 import { logUserIn } from '../../reducers/userSlice';
 import './login.scss';
 
@@ -9,27 +12,35 @@ const Login = ({ dispatch }) => {
   const [userPassword, setUserPassword] = useState('');
 
   const handleOnChange = (e) => {
-    if (e.target.tagName === 'INPUT') {
-      if (e.target.type === 'email') {
-        setUserEmail(e.target.value);
-      } else if (e.target.type === 'password') {
-        setUserPassword(e.target.value);
+    const elem = e.target;
+
+    if (elem.tagName === 'INPUT') {
+      if (elem.type === 'email') {
+        setUserEmail(elem.value);
+      } else if (elem.type === 'password') {
+        setUserPassword(elem.value);
       }
     }
   };
 
   const handleSubmit = () => {
     const params = { email: userEmail, password: userPassword };
-    dispatch(logUserIn(params));
+
+    if (Object.values(params).some((j) => j === '')) {
+      toast.warning('No field can be empty', toastDark);
+    } else {
+      dispatch(logUserIn(params));
+    }
   };
 
   return (
-    <div className="login d-flex align-items-center justify-content-center">
+    <div className="session d-flex align-items-center justify-content-center">
       <div className="form d-flex align-items-center justify-content-center flex-column rounded">
-        <h1>Login</h1>
-        <div className="logindetails d-flex align-items-center flex-column" onChange={handleOnChange}>
+        <h1>Log In</h1>
+        <div className="sessiondetails d-flex align-items-center flex-column" onChange={handleOnChange}>
           <input type="email" className="inputs rounded p-2" placeholder="Email" />
           <input type="password" className="inputs rounded p-2 mt-3" placeholder="Password" />
+          <Link to="/signup" className="mt-3">{'Don\'t have an account? Join us!'}</Link>
           <button type="button" className="button mt-4 rounded" onClick={handleSubmit}>Log in</button>
         </div>
       </div>
