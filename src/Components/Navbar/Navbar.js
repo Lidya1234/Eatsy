5900106c30f7d5059bf5352788461882616598dd
helman101 <helmanandresortegon@gmail.com>
2021-10-11 16:55:23 +0200

    add sticky position to Navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,9 +4,9 @@ import ChevronLeft from '@material-ui/icons/ChevronLeft';
 import './navbar.scss';
 
 const Navbar = () => (
-  <div className="navbar d-flex justify-content-center">
+  <div className="navb d-flex justify-content-center sticky-top">
     <div className="wrapper d-flex justify-content-between align-items-center p-4">
-      <Link to="./Home" className="links"><ChevronLeft className="back" /></Link>
+      <Link to="./" className="links"><ChevronLeft className="back" /></Link>
       <h4 className="links"> Menu </h4>
       <Link to="./Logout" className="links ">Logout</Link>
     </div>
