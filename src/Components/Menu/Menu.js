c30d869d01aadf02c8ad6e9821f8e47678fbf197
helman101 <helmanandresortegon@gmail.com>
2021-10-11 16:54:57 +0200

    fix foods displaying

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,36 +1,35 @@
-import React, { useEffect, useSelector, useDispatch } from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import './menu.scss';
 import Navbar from '../Navbar/Navbar';
+import Food from '../Food/Food';
 import { fetchFoodList } from '../../reducers/foodSlice';
 
-const Menu = () => {
-  const dispatch = useDispatch();
-  const { foodList } = useSelector((state) => state.foodList);
+const Menu = ({ dispatch, food }) => {
   useEffect(() => {
     dispatch(fetchFoodList());
-  }, [dispatch]);
+  }, []);
 
   return (
     <div className="menu">
-      <Navbar />
-      {foodList.map((food) => (
-        <div className="wrapper mx-auto p-4 d-flex justify-content-between align-items-center rounded">
-          <div className="item d-flex">
-            <img src="./background-tree.jpg" className="rounded" alt="" />
-
-            <div className="description d-flex flex-column justify-content-center">
-              <h5>{food.name}</h5>
-              <p>descrition of the food</p>
-
-            </div>
-          </div>
-          <div className="itemPrice d-flex flex-column justify-content-center">
-            <p className="fw-bold fs-4">$56</p>
-          </div>
-        </div>
-      ))}
-
+      <>
+        <Navbar />
+        { food.foodList.length > 0 && food.foodList.map((food) => (
+          <Food key={food.id} food={food} />
+        ))}
+      </>
     </div>
   );
 };
-export default Menu;
+
+Menu.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  food: PropTypes.instanceOf(Object).isRequired,
+};
+
+const mapStateToProps = ({ food }) => ({
+  food,
+});
+
+export default connect(mapStateToProps)(Menu);
