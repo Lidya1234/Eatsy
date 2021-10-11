2e06d83842a6f72ab244cb18e7281f459b020ebe
Lidya1234 <lidya94gher@gmail.com>
2021-10-11 13:11:21 +0300

    Add code to get data from server and displaying it

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useEffect, useSelector, useDispatch } from 'react';
 import './menu.scss';
+import Navbar from '../Navbar/Navbar';
+import { fetchFoodList } from '../../reducers/foodSlice';
 
-const Menu = () => (
-  <div className="menu">
-    <div className="wrapper mx-auto p-4 d-flex justify-content-between align-items-center rounded">
-      <div className="item d-flex">
-        <img src="./background-tree.jpg" className="rounded" alt="" />
+const Menu = () => {
+  const dispatch = useDispatch();
+  const { foodList } = useSelector((state) => state.foodList);
+  useEffect(() => {
+    dispatch(fetchFoodList());
+  }, [dispatch]);
 
-        <div className="description d-flex flex-column justify-content-center">
-          <h5>Name</h5>
-          <p>descrition of the food</p>
+  return (
+    <div className="menu">
+      <Navbar />
+      {foodList.map((food) => (
+        <div className="wrapper mx-auto p-4 d-flex justify-content-between align-items-center rounded">
+          <div className="item d-flex">
+            <img src="./background-tree.jpg" className="rounded" alt="" />
 
+            <div className="description d-flex flex-column justify-content-center">
+              <h5>{food.name}</h5>
+              <p>descrition of the food</p>
+
+            </div>
+          </div>
+          <div className="itemPrice d-flex flex-column justify-content-center">
+            <p className="fw-bold fs-4">$56</p>
+          </div>
         </div>
-      </div>
-      <div className="itemPrice d-flex flex-column justify-content-center">
-        <p className="fw-bold fs-4">$56</p>
-      </div>
+      ))}
+
     </div>
-  </div>
-);
+  );
+};
 export default Menu;
