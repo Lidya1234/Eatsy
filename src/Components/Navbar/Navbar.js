beb090e9bce5b2c8914c165b257059d7d3b5cef4
Lidya1234 <lidya94gher@gmail.com>
2021-10-25 13:33:06 +0300

    Add logout button

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import { logout } from '../../reducers/userSlice';
 import './navbar.scss';
 
-const Navbar = () => (
-  <div className="navb d-flex justify-content-center fixed-top">
-    <div className="nav-wrap d-flex justify-content-between align-items-center p-4">
-      <Link to="./menu" className="links"><ChevronLeft className="back" /></Link>
-      <h4 className="links"> Eatsy </h4>
-      <Link to="./" className="links ">Logout</Link>
-    </div>
-  </div>
-);
+const Navbar = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const handleClick = () => {
+    history.push('./');
+    dispatch(logout);
+    console.log('logedout');
+  };
 
+  return (
+    <div className="navb d-flex justify-content-center fixed-top">
+      <div className="nav-wrap d-flex justify-content-between align-items-center p-4">
+        <Link to="./menu" className="links"><ChevronLeft className="back" /></Link>
+        <h4 className="links"> Eatsy </h4>
+        {/* <Link to="./" className="links ">Logout</Link> */}
+        <button type="submit" className="links" onClick={handleClick}> Logout</button>
+      </div>
+    </div>
+  );
+};
 export default Navbar;
