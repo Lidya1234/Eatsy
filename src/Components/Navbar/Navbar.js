e74c6e4951ff0e1372cc232e5156484e87ffe0d5
helman101 <helmanandresortegon@gmail.com>
2021-10-25 13:03:57 +0200

    solve log out issue

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -6,12 +6,12 @@ import { logout } from '../../reducers/userSlice';
 import './navbar.scss';
 
 const Navbar = () => {
-  const dispatch = useDispatch();
   const history = useHistory();
+  const dispatch = useDispatch();
+
   const handleClick = () => {
+    dispatch(logout());
     history.push('./');
-    dispatch(logout);
-    console.log('logedout');
   };
 
   return (
@@ -19,10 +19,10 @@ const Navbar = () => {
       <div className="nav-wrap d-flex justify-content-between align-items-center p-4">
         <Link to="./menu" className="links"><ChevronLeft className="back" /></Link>
         <h4 className="links"> Eatsy </h4>
-        {/* <Link to="./" className="links ">Logout</Link> */}
         <button type="submit" className="links" onClick={handleClick}> Logout</button>
       </div>
     </div>
   );
 };
+
 export default Navbar;
